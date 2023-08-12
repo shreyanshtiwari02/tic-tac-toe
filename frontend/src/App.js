4823260408ef550af8c6594a671290d355c5ad20
Shrey <shreyansh.tiwari.ug20@nsut.ac.in>
2023-08-12 12:37:27 +0530

    creating a sqaure board

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="square-board">
+      <Square/><Square/><Square/>
+      </div>
+      <div className="square-board">
+      <Square/><Square/><Square/>
+      </div>
+      <div className="square-board">
+      <Square/><Square/><Square/>
+      </div>
     </div>
   );
 }
+
+function Square({value}){
+
+  function handleClick(){
+    console.log("Clicked!");
+  }
+
+
+  return (
+    <button className="square" onClick={handleClick} >{value}</button>
+  );
+
+}
+
 
 export default App;
